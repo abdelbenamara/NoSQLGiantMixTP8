71e67eb1e1508cea384f376d460a33c624a4f308
Abdel-Benamara <abdelb.contact@gmail.com>
2021-06-16 11:48:33 +0200

    OK

---
+++ giant_mix_project/src/Repository/ClientRepository.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Client;
+
+class ClientRepository
+{
+    function connect()
+    {
+
+    }
+
+    function checkUpLogin(string $username, string $password)
+    {
+
+    }
+
+    function persistClient(Client $client)
+    {
+
+    }
+}
--- giant_mix_project/src/Repository/PanierRepository.php
+++ giant_mix_project/src/Repository/PanierRepository.php
@@ -2,8 +2,6 @@
 
 namespace App\Repository;
 
-use App\Entity\Panier;
-
 class PanierRepository
 {
     function connect()
